3110d2a157f7296a927a19319d65500fa4dc5cd9
Jacob Marthaller <marthaller.jr@gmail.com>
2020-11-04 15:53:21 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,13 +1,13 @@
 require 'pry'
 
-def find_item_by_name_in_collection(name, collection)
-  collection.each do |entry|
-    if entry[:item] == name
-      return entry
-    end
-  end
-  nil
-end
+# def find_item_by_name_in_collection(name, collection)
+#   collection.each do |entry|
+#     if entry[:item] == name
+#       return entry
+#     end
+#   end
+#   nil
+# end
 
 
 
@@ -23,9 +23,77 @@ def consolidate_cart(cart)
     end
     i += 1
   end
-   cart
+  cart
 end
 
 
 
+
+
+
+
+
+
+# def find_item_by_name_in_collection(name, collection)
+#   collection.each do |entry|
+#     if entry[:item] == name 
+#       return entry
+#     end
+#   end
+#   nil
+# end
+
+
+
+
+
+# def consolidate_cart(cart)
+#   key_store = cart[0][:item]
+#   cart.each do |entry|
+#     entry[:count] = 1
+    
+#     entry.each do |key, value|
+#       #binding.pry
+#       if key_store == value
+#         entry[:count] += 1
+#       end
+#     end
+#   end
+#   cart
+#   binding.pry
+# end
+
+
+#puts consolidate_cart(cart)
+  
+  
+  
+  
+  
+  # def find_item_by_name_in_collection(name, collection)
+  #   i = 0
+  #   while i < collection.length do 
+  #     if collection[i][:item] == name
+  #       return collection[i]
+  #     end
+  #     i += 1
+  #   end
+  #   nil
+  # end
+  
+  def find_item_by_name_in_collection(name, collection)
+    collection.each do |entry|
+      if entry[:item] == name
+        return entry
+      end
+    end
+    nil
+  end
+  
+  
+  
+  
+  
+  
+  
   
